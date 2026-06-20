5530f2b078cba30716b079e816635238138675c0
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-20 15:37:56 +0530

    Time: 2 ms (96.65%), Space: 48.8 MB (14.15%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] countBits(int n) {
 
-        int ans []=new int [n+1];
-        
-        for( int i=1; i<ans.length;i++){
-                   // built function for the count 1 bit .
-                  ans[i]=Integer.bitCount(i);
+        // we can optimized 
+     int ans []=new int[n+1];
+     if( n == 0) return ans;
+     for( int i=1; i<=n; i++){
+        // if odd number then divid by 2+1;
+        if( i % 2 ==1){
+            ans[i]=ans[i/2]+1;
         }
+        else{
+            ans[i]=ans[i/2];
+        }
+     }
         return ans;
     }
 }
