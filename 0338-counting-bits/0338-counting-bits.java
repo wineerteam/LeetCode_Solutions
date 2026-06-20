8996b653b47cd21d11f993d7db3250692fd0cc9e
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-20 14:08:05 +0530

    Time: 2 ms (96.65%), Space: 48.4 MB (84.5%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -5,11 +5,7 @@ class Solution {
         
         for( int i=1; i<ans.length;i++){
                    // built function for the count 1 bit .
-                   int count =0;
-            for( int j=31; j>=0;j--){
-                if(((i>>j)&1)==1)count++;
-            }
-            ans[i]=count;
+                  ans[i]=Integer.bitCount(i);
         }
         return ans;
     }
