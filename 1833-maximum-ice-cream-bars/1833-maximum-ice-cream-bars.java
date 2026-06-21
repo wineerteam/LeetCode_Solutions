69bdb21f78a64ebd5cb5dcefc7d2acd3d7ddf839
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-21 17:46:48 +0530

    Time: 39 ms (7.12%), Space: 83.7 MB (68.89%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -4,10 +4,10 @@ class Solution {
         int count=0;
         int sum =0;
         if(arr[0]>coins) return 0;
-        for( int num:arr){
-            if(num<coins && sum+num<=coins){
+        for( int i=0; i<arr.length; i++){
+            if(arr[i]<coins && sum+arr[i]<=coins){
                 count++;
-                sum+=num;
+                sum+=arr[i];
             }
             if(sum >=coins) break;
             
