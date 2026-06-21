6ad1abe4c550b942d6d5b86be7a836aaf6aab6ee
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-21 19:59:47 +0530

    Time: 1 ms (92.77%), Space: 47.4 MB (32.45%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -5,10 +5,17 @@ class Solution {
         int pre=1 , suf=1;
         int n=nums.length;
         for(int i=0; i<n; i++){
+            
+            // if zoro foun in subarray then reset the value
+            // reset kar region h. zero value in subarray complete zero kr dega 
             if(pre==0)pre=1;
             if(suf==0)suf=1;
+            // find product left side with rigth side and jo max ho include 
+            // left or right side se find krne ka logic h becasue of -ve  in odd times 
             pre *=nums[i];
             suf *=nums[n-1-i];
+
+            // inlcude max it can be prefix side or sufix side 
             ans=Math.max(ans,Math.max(pre,suf));
         }
         return ans;
