32d16e168310affae942a94e9b3f2496544e9093
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-21 19:55:21 +0530

    Time: 1 ms (92.77%), Space: 47.2 MB (71.8%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        int maxPro=Integer.MIN_VALUE;
-        int pro;
+        int ans = Integer.MIN_VALUE;
+        int pre=1 , suf=1;
         int n=nums.length;
-        
-        for( int i=0; i<n; i++){
-                pro=1;
-            for(int j=i; j<n; j++){
-                pro *=nums[j];
-                maxPro=Math.max(maxPro,pro);
-            }
+        for(int i=0; i<n; i++){
+            if(pre==0)pre=1;
+            if(suf==0)suf=1;
+            pre *=nums[i];
+            suf *=nums[n-1-i];
+            ans=Math.max(ans,Math.max(pre,suf));
         }
-return maxPro;
-        
+        return ans;
     }
 }
-
-//  The Time Complexity is O(N^3)
-//  The Time Complexity is O(N^2)
-//  
