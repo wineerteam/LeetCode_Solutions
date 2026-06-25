0f005e97f3c0ba278349753e303e9566a4b3a4f3
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-25 22:00:32 +0530

    Time: 1 ms (99.95%), Space: 77.4 MB (31.64%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+
+        int maxi=Integer.MIN_VALUE;
+        int sum=0;
+        
+        for( int i=0; i<arr.length; i++){
+            
+             sum +=arr[i];
+
+             if( maxi < sum ) maxi=sum;
+             if( sum < 0) sum=0;
+        }
+        
+           
+        return maxi;
+    }
+}
+
+// 1. brute forces  Tc--o(n^3)
+        // 2. we can better solution Tc--o(n^2)
+        // 3. we can optimize this problem  Tc--o(n)
+        // 4. Let me Explain it... 
