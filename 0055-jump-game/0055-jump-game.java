f9becd03fefe179bfc4597674d0c18ceab06021e
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-26 12:04:08 +0530

    Time: 37 ms (12.47%), Space: 48.3 MB (8.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean canJump(int[] nums) {
 
-        // one vairbale use so one d aray sufficent 
+        int n = nums.length;
+        // use to hme i-1 tak pahucha skta h ya nhi ye store karega 
+        boolean ans[]=new boolean[n];
+        // store all false wase by default bhi to whi hota h.
+
+        Arrays.fill(ans,false);
         
-        boolean arr[]=new boolean [nums.length];
-         arr[0]=true;
-        for( int i=1; i<nums.length;i++){
-            for( int k=i-1; k>=0; k--){
-                if(arr[k]==true && k+nums[k] >=i){
-                    arr[i]=true;
+        // accroding to questions we reach 1st posstion so intialize 
+        ans[0]=true;
+        
+        for( int i=1; i<n; i++){
+            // check i-1 to zero tk
+            for(int j=i-1; j>=0; j--){
+                // check it i-1 tk khud pahuch skta h. or hme i tk pahucha skta both condition try kro
+                if( ans[j]==true && j+nums[j] >= i) {
+                    ans[i]=true;
                     break;
                 }
             }
         }
-        return arr[nums.length-1];
+        return ans[n-1];
     }
 }
