645eeaaca35d2fecbc2db8dca7852a670435de93
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-07-24 06:40:05 +0530

    Time: 0 ms (100%), Space: 125.4 MB (54.9%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        if (n <= 2)
+            return n;
+
+        int ans = 1;
+
+        while (ans <= n) {
+            ans <<= 1;
+        }
+
+        return ans;
+    }
+}
