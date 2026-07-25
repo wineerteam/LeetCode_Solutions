cc6f38920cd6b380df7176167ded47a4a1de4cf3
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-07-26 00:25:06 +0530

    Time: 0 ms (100%), Space: 14.6 MB (53.4%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+private: 
+    void dfs(int node, vector<int>&vis,vector<vector<int>>&adj){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,vis,adj);
+            }
+        }
+    }
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        // pahle dfs use krte h
+        int n=rooms.size();
+        vector<int>vis(n,0);
+        dfs(0,vis,rooms);
+        for(auto it:vis){
+            if(it==0)return false;
+        }
+        return true;
+    }
+
+};
