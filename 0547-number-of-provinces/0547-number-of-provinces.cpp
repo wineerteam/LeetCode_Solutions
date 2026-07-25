8d384f8ca7874f4d63a5c95a80c95f117ee206df
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-07-26 00:50:34 +0530

    Time: 5 ms (15.93%), Space: 21.3 MB (6.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,12 +1,20 @@
 class Solution {
 private:
-    void dfs(int node, vector<int>&vis,vector<vector<int>>&adj){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                dfs(it,vis,adj);
+    void bfs (int i, vector<int>&vis,vector<vector<int>>&adj){
+        vis[i]=1;
+        queue<int>q;
+        q.push(i);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
             }
         }
+        
     }
 public:
     int findCircleNum(vector<vector<int>>& mat) {
@@ -32,9 +40,11 @@ public:
         vector<int>vis(adj.size(),0);
         for(int i=0; i<adj.size(); i++){
             if(!vis[i]){
-            dfs(i,vis,adj);
+             bfs(i,vis,adj);
             cnt++;}
         } 
         return cnt;       
     }
 };
+
+// es me just hme bfs and dfs call krna h. or visite krna h. ek external for loop se ek bar loop start hoga to jb tk conneceted hoga tb  tak chalega or agar diconnect huta to fir se bfs call hoga or cnt increaes hoga
