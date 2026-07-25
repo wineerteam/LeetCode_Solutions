6a782be158934dcfa9c196c789f18aca3fe4c34e
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-07-26 00:10:49 +0530

    Time: 231 ms (86.59%), Space: 285.6 MB (69.14%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,35 +1,37 @@
 class Solution {
-private:
-    bool dfs(vector<int>&vis,vector<vector<int>>&adj,int node, int dest){
-         if( node==dest){
-            return true;
-         }
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if( !vis[it]){
-                vis[it]=1;
-                if(dfs(vis,adj,it,dest)) 
-                return true;
-            }
-        }
-
-return false;
-    }
 public:
     bool validPath(int n, vector<vector<int>>& edges, int src, int dest) {
         
-        vector<vector<int>>adj(n);
-        for(auto &it:edges){
+        // converte into adj list
+        vector<vector<int>> adj(n);
+        for( auto &it:edges){
             int u=it[0];
             int v=it[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-         // dfs 
-         vector<int>vis(n,0);
-         
-        return dfs(vis,adj,src,dest);
+        // queue 
+        queue<int>q;
+        vector<int>vis(n,0);
+        q.push(src);
+        vis[src]=1;
 
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if( node==dest)return true;
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
             }
+        }
+
+
+
+
+        return false;
+    }
 };
