2bb77f2b29783f55ea31d2a94ca914c15b702268
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-09-01 09:08:32 +0530

    Time: 0 ms (100%), Space: 26.3 MB (37.63%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+
+        int ans=arr[0];
+       for(int i=1; i<arr.size(); i++){
+            
+            ans=ans^arr[i];
+       }
+        return ans;
+    }
+};
