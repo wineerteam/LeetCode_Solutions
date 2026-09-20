4d5cc8b637381756c31b159715a5045d3c363338
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-09-21 02:50:27 +0530

    Time: 35 ms (10.3%), Space: 12.8 MB (33.35%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,28 +1,31 @@
 class Solution {
-
-public:
-vector<vector<int>>ans;
-set<vector<int>>st;
-int n;
-void solve(int idx, vector<int> &arr){
-    if( idx==n){
-        if(st.find(arr)==st.end()){
+    
+    vector<vector<int>>ans;
+    set<vector<int>>st;
+    int n;
+    
+    void solve(int idx, vector<int> &arr){
+        if( idx==n){
+           if( st.find(arr)==st.end()){
             st.insert(arr);
-             ans.push_back(arr); 
+            ans.push_back(arr);
+            return ;        
+               }
         }
-        return ;
-    }
-    for( int i=idx; i<n; i++){
-        swap(arr[i],arr[idx]);
+
+               for( int i=idx; i<n; i++){
+                swap(arr[i],arr[idx]);
+                solve(idx+1, arr);
+                swap(arr[i],arr[idx]);
+               }
+        }
+    
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+       n =nums.size();
+       solve(0,nums);
+       return ans;
         
-        solve(idx+1,arr);
-        swap(arr[i],arr[idx]);
-    }
-}
-    vector<vector<int>> permuteUnique(vector<int>& arr) {
-         
-          n=arr.size();
-         solve(0,arr);
-         return ans;
     }
 };
