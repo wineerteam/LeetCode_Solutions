218d260f4b369ed6059af2b585ed7faa5e439c15
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-09-21 11:29:55 +0530

    Time: 7 ms (24.04%), Space: 14.4 MB (38.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    public:
-    void solve(vector<int>&arr,int tar,vector<vector<int>>&ans,vector<int>&curr,int i){
-         if( i>=arr.size() || tar<0) return ;
-         if(tar == 0){
-            ans.push_back(curr);
-            return;
-         }
-        curr.push_back(arr[i]);
-        solve(arr,tar-arr[i],ans,curr,i);
-        // non take
-        curr.pop_back();
-        solve(arr,tar,ans,curr,i+1);
+    vector<vector<int>>ans;
+    void solve(int idx,vector<int>&arr,int &tar,vector<int>&temp){
+               if( idx>=arr.size()|| tar<0){
+                return ;
+               }
+               if( tar==0){
+                ans.push_back(temp);
+                return ;
+               }
+                
 
+               for(int i=idx; i<arr.size(); i++){
+                  temp.push_back(arr[i]);
+                  tar=tar-arr[i];
+                  solve(i,arr,tar,temp);
+                  tar=tar+arr[i];
+                  temp.pop_back();
+               }
     }
+
 public:
-    vector<vector<int>> combinationSum(vector<int>& cnd, int tar) {
-
-        vector<vector<int>>ans;
-        vector<int>curr;
-        solve(cnd,tar,ans,curr,0);
+    vector<vector<int>> combinationSum(vector<int>& arr, int tar) {
+        vector<int>temp;
+        solve(0,arr,tar,temp);
         return ans;
-
-        
     }
 };
