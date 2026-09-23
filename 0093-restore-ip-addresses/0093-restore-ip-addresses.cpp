91f22cf70999aa5d38dc3ca53c7fb2621c406d0b
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-09-23 23:46:42 +0530

    Time: 42 ms (5.04%), Space: 41.3 MB (5.06%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,62 +1,47 @@
 class Solution {
-    int cnt;
-    vector<string> ans;
+    vector<string>ans;
+    int n;
 
-    bool isValid(string s) {
-        int n = s.length();
-        if (n == 0 || n > 3)
-            return false;
-
-        
-        if (n > 1 && s[0] == '0')
-            return false;
-        int num = stoi(s);
-        return num <= 255;
+    bool isValid(string str){
+        if(  str[0]=='0') return false;
+        int num=stoi(str);
+        if(num>255) return false;
+        return true;
     }
 
-    void solve(string &s, int i, string &temp) {
-
-        if (cnt < 0 || i >= s.length())
-            return;
-        if (cnt == 0) {
-            string last = s.substr(i);
-            if (isValid(last)) {
-                ans.push_back(temp + last);
-            }
-            return;
-        } 
-        string part = "";
-
-        for (int j = i; j < s.length() && j < i + 3; j++) {
-
-            part.push_back(s[j]);
-
-            if (!isValid(part))
-                continue;
-
-            // take
-            temp += part;
-            temp.push_back('.');
-
-            cnt--;
-            solve(s, j + 1, temp);
-            cnt++;
-
-        //  undo kro
+    void solve(string &s, int idx, string temp, int parts){
+        
+        
+        if( idx==s.length() && parts==4) {
             temp.pop_back();
-            temp.erase(temp.size() - part.size());
+            ans.push_back(temp);
+            return ;
         }
+
+        // solve teke one int
+        if(idx+1<=n)
+        solve(s,idx+1,temp+s.substr(idx,1)+".",parts+1);
+        
+        // solve take two digit
+         if(idx+2<=n && isValid(s.substr(idx,2)) )
+         solve(s,idx+2,temp+s.substr(idx,2)+".",parts+1);
+
+        // solve take three digit
+        if(idx+3<=n && isValid(s.substr(idx,3)) )
+         solve(s,idx+3,temp+s.substr(idx,3)+".",parts+1);
+
     }
 
 public:
     vector<string> restoreIpAddresses(string s) {
-
-        if (s.length() < 4 || s.length() > 12)
-            return ans;
-        cnt = 3;
-        string temp = "";
-        solve(s, 0, temp);
-
+        ans.clear();
+         n=s.length();
+        // if( n>12) return ans ;
+        string temp="";
+        int parts=0;
+        solve(s,0,temp,parts);
         return ans;
+
+        
     }
 };
